429ef0050ac2ebfcb5256305b4876044a5fdf9db
krispycrem <116591043+krispycrem@users.noreply.github.com>
2023-03-27 23:54:54 -0400

    Update Compute.java

--- Bluck/src/Compute.java
+++ Bluck/src/Compute.java
@@ -7,7 +7,6 @@ public class Compute implements AM {
         left = info.parent.readLong();
         right = info.parent.readLong();
         long sum = 0;
-        System.out.println("Worker started");
         for (long n = left; n < right + 1; n++) {
             for (long i = 1; i < n / 4 + 1; i++) {
                 if (i * i * i * i == n) {
